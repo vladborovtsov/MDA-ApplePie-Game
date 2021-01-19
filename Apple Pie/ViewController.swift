8ff85575234f307cbc6c255a47cf1d7550e8467d
Vlad Borovtsov <borovcov.vlad@gmail.com>
2021-01-19 13:46:49 +0700

    Initial Commit

---
+++ Apple Pie/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Apple Pie
+//
+//  Created by Vlad Borovtsov on 19.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
