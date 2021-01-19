87c692a3d08d9ede6db90a5d7ca86dfc64a55703
Vlad Borovtsov <borovcov.vlad@gmail.com>
2021-01-19 17:06:53 +0700

    added game model and some basic UI update code

---
+++ Apple Pie/Game.swift
@@ -0,0 +1,11 @@
+//
+//  Game.swift
+//  Apple Pie
+//
+//  Created by Vlad Borovtsov on 19.01.2021.
+//
+
+struct Game {
+  var word: String
+  var incorrectMovesRemaining: Int
+}
--- Apple Pie/ViewController.swift
+++ Apple Pie/ViewController.swift
@@ -15,10 +15,116 @@ class ViewController: UIViewController {
   
   @IBOutlet weak var scoreLabel: UILabel!
   @IBOutlet weak var correctWordLabel: UILabel!
+  
+  // MARK: - Properties
+  var totalWins = 0;
+  var totalLosses = 0;
+  
+  var currentGame: Game!
+  let incorrectMovesAllowed = 7;
+  var listOfWords = [
+    "Александрия",
+    "Атланта",
+    "Ахмедабад",
+    "Багдад",
+    "Бангалор",
+    "Бангкок",
+    "Барселона",
+    "Белу-Оризонти",
+    "Богота",
+    "Буэнос-Айрес",
+    "Вашингтон",
+    "Гвадалахара",
+    "Гонконг",
+    "Гуанчжоу",
+    "Дакка",
+    "Даллас",
+    "Далянь",
+    "Дар-эс-Салам",
+    "Дели",
+    "Джакарта",
+    "Дунгуань",
+    "Йоханнесбург",
+    "Каир",
+    "Калькутта",
+    "Карачи",
+    "Киншаса",
+    "Куала Лумпур",
+    "Лагос",
+    "Лахор",
+    "Лима",
+    "Лондон",
+    "Лос-Анджелес",
+    "Луанда",
+    "Мадрид",
+    "Майами",
+    "Манила",
+    "Мехико",
+    "Москва",
+    "Мумбаи",
+    "Нагоя",
+    "Нанкин",
+    "Нью-Йорк",
+    "Осака",
+    "Париж",
+    "Пекин",
+    "Пуна",
+    "Рио-де-Жанейро",
+    "Сан-Паулу",
+    "Санкт-Петербург",
+    "Сантьяго",
+    "Сеул",
+    "Сиань",
+    "Сингапур",
+    "Стамбул",
+    "Сурат",
+    "Сучжоу",
+    "Тегеран",
+    "Токио",
+    "Торонто",
+    "Тяньцзинь",
+    "Ухань",
+    "Филадельфия",
+    "Фошань",
+    "Фукуока",
+    "Хайдарабад",
+    "Ханчжоу",
+    "Харбин",
+    "Хартум",
+    "Хошимин",
+    "Хьюстон",
+    "Цзинань",
+    "Циндао",
+    "Ченнай",
+    "Чикаго",
+    "Чунцин",
+    "Чэнду",
+    "Шанхай",
+    "Шэньчжэнь",
+    "Шэньян",
+    "Эр-Рияд",
+    "Янгон"
+  ];
+  
+  // MARK: - Methods
+  func newRound() {
+    let newWord = listOfWords.removeFirst();
+    currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed);
+    updateUI();
+  }
+
+  func updateUI() {
+    let movesRemaining = currentGame.incorrectMovesRemaining;
+    let imageName = "Tree0\(movesRemaining < 8 ? movesRemaining : 7)"; //we are using 7 here bcs Tree07 is max. available image.
+    treeImageView.image = UIImage.init(named: imageName);
+    scoreLabel.text = "Выигрыши: \(totalWins), Проигрыши: \(totalLosses)";
+  }
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-  
+    newRound();
   }
+  
 
   // MARK: - IB Actions
   
